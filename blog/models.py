683bb89f9a2b85bb38be2253b6650c2f377dfd90
behnambm <behnam.mohamadzadeh21@gmail.com>
2021-03-23 14:24:28 +0430

    add `author` field to Article model

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from django.utils import timezone
 
+from account.models import User
+
 
 class Article(models.Model):
+    class Meta:
+        ordering = ('-publish', )
     CHOICES = (
         ('published', 'منتشر شده'),
         ('draft', 'پیش نویس'),
         ('pending', 'در حال بررسی'),
         ('rejected', 'رد شده'),
     )
-
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     content = models.TextField()
@@ -18,3 +22,6 @@ class Article(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     publish = models.DateTimeField(default=timezone.now)
     status = models.CharField(choices=CHOICES, max_length=100)
+
+    def __str__(self):
+        return self.title
