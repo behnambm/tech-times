c9b6c54b59a3f8bb382ec2a1654710c45a627704
behnambm <behnam.mohamadzadeh21@gmail.com>
2021-03-24 22:05:53 +0430

    add Article Detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from .views import ArticleListView
+from .views import (
+    ArticleListView,
+    ArticleDetailView,
+    AuthorProfileView,
+)
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='home'),
     path('page/<int:page>/', ArticleListView.as_view(), name='home'),
+
+    path('article/<slug:slug>/', ArticleDetailView.as_view(), name='detail'),
+
+    path('author/<usernmae>/', AuthorProfileView.as_view(), name='author'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.shortcuts import get_object_or_404
 
 from .models import Article
 
@@ -8,3 +10,12 @@ class ArticleListView(ListView):
     queryset = Article.objects.published()
     paginate_by = settings.PAGINATE_COUNT
 
+
+class ArticleDetailView(DetailView):
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Article.objects.published(), slug=self.kwargs.get('slug'))
+
+
+class AuthorProfileView(DetailView):
+    pass
