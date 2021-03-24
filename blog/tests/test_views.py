b3d63886a34e7bba95ef260c19bc332b0e038b89
behnambm <behnam.mohamadzadeh21@gmail.com>
2021-03-24 11:10:37 +0430

    restructure and add one more unittest to ViewTest

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -1,12 +1,21 @@
+from django.urls import reverse
+from django.conf import settings
+
 from . import BaseTestCase
 
 
 class ViewsTestCase(BaseTestCase):
-    def test_home_loads_properly(self):
-        response = self.client.get('http://127.0.0.1:8000')
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        self.home_url = reverse('blog:home')
+        self.home_response = self.client.get(self.home_url)
 
-    def test_home_context(self):
-        response = self.client.get('http://127.0.0.1:8000')
-        articles = response.context_data['object_list']
+    def test_home_loads_properly_and_uses_right_template(self):
+        self.assertEqual(self.home_response.status_code, 200)
+        self.assertTemplateUsed(self.home_response, 'blog/article_list.html')
+
+    def test_article_model_ordering(self):
+        articles = self.home_response.context_data['object_list']
         self.assertEqual(articles[0].title, 'Test Article 11')
+
+    def test_home_paginate_count(self):
+        self.assertEqual(len(self.home_response.context_data['object_list']), settings.PAGINATE_COUNT)
