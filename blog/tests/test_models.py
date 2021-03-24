3ee95bdff0e0c4908eea800e366df0c3ae2b7e06
behnambm <behnam.mohamadzadeh21@gmail.com>
2021-03-24 14:11:26 +0430

    ✅ add unittest for published articles

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -3,6 +3,10 @@ from . import BaseTestCase
 from ..models import Article
 from account.models import User
 
+"""
+All pre-populated data for testing are inside __init__.py.BaseTestCase.setUpTestData
+"""
+
 
 class ModelsTestCase(BaseTestCase):
     def test_article_user_models_connected(self):
--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -3,6 +3,10 @@ from django.conf import settings
 
 from . import BaseTestCase
 
+"""
+All pre-populated data for testing are inside __init__.py.BaseTestCase.setUpTestData
+"""
+
 
 class ViewsTestCase(BaseTestCase):
     def setUp(self):
@@ -15,7 +19,11 @@ class ViewsTestCase(BaseTestCase):
 
     def test_article_model_ordering(self):
         articles = self.home_response.context_data['object_list']
-        self.assertEqual(articles[0].title, 'Test Article 11')
+        self.assertEqual(articles[0].title, 'Test Article 10')
 
     def test_home_paginate_count(self):
         self.assertEqual(len(self.home_response.context_data['object_list']), settings.PAGINATE_COUNT)
+
+    def test_home_doesnt_have_unpublished_article(self):
+        articles_title_list = [i.title for i in self.home_response.context_data['object_list']]
+        self.assertNotIn('Test Article 11', articles_title_list)
