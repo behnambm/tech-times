62ac379af4ce1a58e3d858b53813ba21e4b1b7e2
behnambm <behnam.mohamadzadeh21@gmail.com>
2021-03-26 13:32:58 +0430

    add Bio to user model

--- account/models.py
+++ account/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    bio = models.CharField(max_length=300, verbose_name='درباره نویسنده', null=True, blank=True)
 
