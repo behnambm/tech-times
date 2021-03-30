f3307ab2b42ff0cd5f54a72fcc007dabc557a83c
behnambm <behnam.mohamadzadeh21@gmail.com>
2021-03-30 16:08:19 +0430

    add my User registration form

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.forms import AuthenticationForm, UsernameField
 from django import forms
 from django.utils.translation import gettext, gettext_lazy as _
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import User
 
 
 class AccountAuthenticationForm(AuthenticationForm):
@@ -25,3 +28,18 @@ class AccountAuthenticationForm(AuthenticationForm):
             }
         ),
     )
+
+
+class UserRegistrationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs.update({'placeholder': 'نام کاربری'})
+        self.fields['email'].widget.attrs.update({'placeholder': 'ایمیل'})
+        self.fields['email'].required = True
+        self.fields['password1'].widget.attrs.update({'placeholder': 'رمزعبور'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'تکرار رمزعبور'})
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
