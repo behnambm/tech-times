4433fb3eca93f8c768569b717fdd93f2da47f606
behnambm <behnam.mohamadzadeh21@gmail.com>
2021-04-01 15:45:48 +0430

    add `is_author` & make email unique in Users model

--- account/models.py
+++ account/models.py
@@ -4,4 +4,5 @@ from django.db import models
 
 class User(AbstractUser):
     bio = models.CharField(max_length=300, verbose_name='درباره نویسنده', null=True, blank=True)
-
+    email = models.EmailField(unique=True, verbose_name='آدرس ایمیل')
+    is_author = models.BooleanField(default=False, verbose_name='کاربر نویسنده')
