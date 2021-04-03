1adde244c64c93e717421a72a3dc3426338258a9
behnambm <behnam.mohamadzadeh21@gmail.com>
2021-04-03 13:07:54 +0430

    add verbose names to Article model's fields

--- blog/models.py
+++ blog/models.py
@@ -21,15 +21,15 @@ class Article(models.Model):
         ('pending', 'در حال بررسی'),
         ('rejected', 'رد شده'),
     )
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles')
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    content = models.TextField()
-    thumbnail = models.ImageField(upload_to='images')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles', verbose_name='نویسنده')
+    title = models.CharField(max_length=200, verbose_name='عنوان')
+    slug = models.SlugField(unique=True, verbose_name='آدرس')
+    content = models.TextField(verbose_name='محتوا')
+    thumbnail = models.ImageField(upload_to='images', verbose_name='تصویر')
     created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-    publish = models.DateTimeField(default=timezone.now)
-    status = models.CharField(choices=CHOICES, max_length=100)
+    publish = models.DateTimeField(default=timezone.now, verbose_name='زمان انتشار')
+    status = models.CharField(choices=CHOICES, max_length=100, verbose_name='وضعیت')
 
     def jpublish(self):
         """This will take datetime and return in jalali format"""
