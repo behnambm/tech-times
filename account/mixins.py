63b57dd50784b1396d2ba446ca7cc03cde2c58b0
behnambm <behnam.mohamadzadeh21@gmail.com>
2021-04-04 23:20:15 +0430

    prevent non-author users to access article creation view

--- account/mixins.py
+++ account/mixins.py
@@ -1,7 +1,12 @@
+from django.http import Http404
+
 
 class AuthorAccessMixin:
     def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_author:
+            raise Http404()
         self.fields = ['title', 'slug', 'content', 'thumbnail', 'publish', ]
         if request.user.is_superuser:
             self.fields += ['status', 'author']
+
         return super().dispatch(request, *args, **kwargs)
