505509d5ef38e95692d6f8a8311f6697898af746
behnambm <behnam.mohamadzadeh21@gmail.com>
2021-04-11 19:40:18 +0430

    add urls to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool, default=False)
 DEVELOPMENT = config('DEVELOPMENT', cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
